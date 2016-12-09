a6fa45f2f49efbe6af1cadaf0fea37d4cb4411d6
elemementor <elemementor@gmail.com>
2016-12-10 00:44:30 +0100

    chore(CI): add travis ci

---
+++ utils/deploy-ghpages.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+(
+ cd dist
+ git init
+ git config user.name "Travis-CI"
+ git config user.email "travis@nodemeatspace.com"
+ git add .
+ git commit -m "Deployed to Github Pages"
+ git push --force --quiet "https://${GH_USERNAME}:${GH_PASSWORD}@github.com/wc-catalogue/blaze-elements.git/" master:gh-pages > /dev/null 2>&1
+)
