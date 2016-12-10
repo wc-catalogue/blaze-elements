28cd57fea889956f2cf2dc8e017f0ac28f713e96
elemementor <elemementor@gmail.com>
2016-12-10 02:43:34 +0100

    chore(CI): fix git setup for npm publish

--- utils/publish-packages.sh
+++ utils/publish-packages.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 (
+  git remote set-url origin https://${GH_USERNAME}:${GH_PASSWORD}@github.com/wc-catalogue/blaze-elements.git
+  git config user.name "Travis-CI"
+  git config user.email "travis@nodemeatspace.com"
   echo "//registry.npmjs.org/:_authToken=$NPM_KEY" >> ~/.npmrc
   npm version patch
   npm publish
