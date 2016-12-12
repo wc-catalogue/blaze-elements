e6c1778f61f71744fef727d1206b08fef107eaa1
hopkins-tk <KingT@seznam.cz>
2016-12-12 23:04:53 +0100

    fix(polyfills): fixes polyfills for IE (#5)

--- polyfills.ts
+++ polyfills.ts
@@ -19,8 +19,9 @@ function _inherits(subClass, superClass) {
 
 (window as any).__extends = _inherits;
 
-import 'skatejs-web-components';
-
 // Added parts of es6 which are necessary for your project or your browser support requirements.
 import 'core-js/es6/object';
+import 'core-js/es6/array';
 import 'core-js/es6/promise';
+
+import 'skatejs-web-components';
