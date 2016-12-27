2bdffd38398c18a02594c1236416fba3a526c8e2
hopkins-tk <KingT@seznam.cz>
2016-12-27 12:17:51 +0100

    Range element introduced (#31)
    * feat(range): range element introduced
    
    * feat(range): added support for color

---
+++ packages/range/Range.demo.tsx
@@ -0,0 +1,79 @@
+import { h, Component, prop } from 'skatejs';
+import { Range } from './Range';
+
+export class Demo extends Component<void> {
+  static get is() { return 'bl-range-demo' }
+  static get props() {
+    return {
+      val: prop.number()
+    }
+  }
+
+  val: number;
+
+  private reflectValue( event: Event ) {
+    this.val = Number.parseInt((event.currentTarget as HTMLInputElement).value);
+  }
+
+  connectedCallback(){
+    super.connectedCallback();
+    this.reflectValue = this.reflectValue.bind(this);
+  }
+
+  renderCallback() {
+    return [
+      <style></style>,
+      <fieldset>
+        <legend>{Range.is}</legend>
+
+        <div>
+          1 &mdash; 10
+          <Range color="error"
+                 value="5"
+                 min="1"
+                 max="10"
+                 onKeyup={() => { console.log("onKeyUp")}}
+                 onFocus={() => { console.log("onFocus")}}
+                 onBlur={() => { console.log("onBlur")}}
+                 onChange={this.reflectValue}
+          ></Range>
+          Current value: {this.val}
+
+          <br />
+
+          0 &mdash; 100
+          <Range color="warning"
+                 value="25"
+          ></Range>
+
+          <br />
+
+          0 &mdash; 100
+          <Range color="success"
+                 value="33"
+          ></Range>
+
+          <br />
+
+          0 &mdash; 10
+          <Range color="info"
+                 max="10"
+                 value="5"
+          ></Range>
+
+          <br />
+
+          1 &mdash; 10
+          <Range value="5"
+                 min="1"
+                 max="10"
+                 disabled
+          ></Range>
+        </div>
+
+      </fieldset>
+    ]
+  }
+}
+
+customElements.define( Demo.is, Demo );
---
+++ packages/range/Range.tsx
@@ -0,0 +1,78 @@
+import { h, Component, prop, emit } from 'skatejs';
+import styles from './Range.scss';
+import {ColorType, cssClassForColorType} from '../utils/colorTypes'
+import { css } from '../../utils/css';
+
+
+//public
+interface RangeProps extends JSX.HTMLProps<HTMLInputElement | any> {
+  color?: keyof ColorType,
+}
+
+export class Range extends Component<RangeProps> {
+  _props: RangeProps;
+  static get is() { return 'bl-range' }
+  static get props() {
+    return {
+      value: prop.number({
+        attribute: true
+      }),
+      min: prop.number({
+        attribute: true
+      }),
+      max: prop.number({
+        attribute: true
+      }),
+      color: prop.string({
+        attribute: true
+      }),
+      disabled: prop.boolean({
+        attribute: true
+      }),
+    }
+  }
+
+  color: ColorType;
+  value: number;
+  min = 0;
+  max = 100;
+  disabled: boolean;
+
+  inputElement: HTMLInputElement;
+
+  private provideValue(event: Event) {
+    const oldValue = this.value;
+    this.value = this.inputElement.valueAsNumber;
+    emit(this,'change'); // emit change event on root element
+  }
+
+  connectedCallback(){
+    super.connectedCallback();
+    this.provideValue = this.provideValue.bind(this);
+  }
+
+  renderCallback() {
+    const { color, value, min, max, disabled } = this;
+    const colorClass = cssClassForColorType('c-range', color);
+    const className = css('c-range', colorClass);
+
+    return [
+      <style>{styles}</style>,
+      <input
+        ref={(_ref: HTMLInputElement)=>this.inputElement=_ref}
+        className={className}
+        type="range"
+        value={value ? value.toString() : null}
+        min={min}
+        max={max}
+        onChange={this.provideValue}
+        disabled={disabled}
+      />
+    ]
+  }
+}
+
+
+customElements.define( Range.is, Range );
+
+
