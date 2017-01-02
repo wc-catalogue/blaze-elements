d92ae41c2a1ae25eaf60b3440c8e1e6977c591bb
hopkins-tk <KingT@seznam.cz>
2017-01-02 13:19:43 +0100

    Refactoring of typography module (#57)
    * feat(typography): refactoring of typography module
    
    * fix(typography): code review comments
    
    * fix(typography): code review comments

--- toolbelt/generate-packages-list.js
+++ toolbelt/generate-packages-list.js
@@ -9,11 +9,7 @@ console.log( packageJson.packages );
 fs.writeFileSync( './package.json', JSON.stringify( packageJson, null, 2) + "\n" );
 
 const indexFile = packageJson.packages.map( ( packageName ) => `import './${ packageName }/index';` ).join( "\n" );
-const indexDemoFile = packageJson.packages.map( ( packageName ) => `import './${ packageName }/${ makeCamelCase( packageName ) }.demo';` ).join( "\n" );
+const indexDemoFile = packageJson.packages.map( ( packageName ) => `import './${ packageName }/index.demo';` ).join( "\n" );
 
 fs.writeFileSync( './packages/index.ts', indexFile + "\n" );
 fs.writeFileSync( './packages/index.demo.ts', indexDemoFile + "\n" );
-
-function makeCamelCase(string ) {
-    return string.charAt(0).toUpperCase() + string.slice(1).replace(/-([a-z])/g, function (g) { return g[1].toUpperCase(); });
-}
