537a6867e9dff580196e65f036c9ed682cab9e41
david.rus <david.rus@embedit.cz>
2017-01-05 13:19:42 +0100

    feat(tags): add tag selector

--- packages/tag/TagSelector.tsx
+++ packages/tag/TagSelector.tsx
@@ -9,26 +9,28 @@ interface TagSelectorProps extends JSX.HTMLProps<HTMLElement | any> {
   onTagChange?: Function,
   onTagAdd?: Function,
   delimiter?: string,
-  tags?: string[]
+  tags?: any
 }
 
 export class TagSelector extends Component<TagSelectorProps> {
   static get is() { return 'bl-tag-selector' }
   static get props() {
     return {
-      tags: prop.array(),
-      delimiter: prop.string(),
+      tags: prop.array({
+        attribute: true
+      })
     }
   }
 
   inputValue: string;
+  inputElement: HTMLInputElement = null;
+  tags = [];
 
   private handleTagSelectorSearch( event ) {
     if ( event.which === 13 ) {
-      emit( this, 'tagAdd' );
+      emit( this, 'tagChange' );
       event.target.value = '';
     }
-    emit( this, 'tagChange' );
   }
 
   connectedCallback() {
@@ -36,11 +38,8 @@ export class TagSelector extends Component<TagSelectorProps> {
     this.handleTagSelectorSearch = this.handleTagSelectorSearch.bind( this );
   }
 
-  inputElement = null;
-  tags = [];
-
   renderCallback() {
-
+    console.log( 'tags:', this.tags );
     const tags = this.tags.map(tag => {
       return <Tag>{tag}</Tag>;
     });
@@ -49,13 +48,13 @@ export class TagSelector extends Component<TagSelectorProps> {
       <style>{styles}</style>,
       <div class="c-tags">
         <div class="c-tags__container">
-          {{tags}}
+          {tags}
         </div>
         <div class="c-tags__field-container">
           <input class="c-field"
                  onKeyup={this.handleTagSelectorSearch}
                  value={this.inputValue}
-                 ref={(_ref: HTMLDivElement)=>this.inputElement=_ref}/>
+                 ref={(_ref: HTMLInputElement)=>this.inputElement=_ref}/>
         </div>
       </div>
     ]
--- packages/tag/index.demo.tsx
+++ packages/tag/index.demo.tsx
@@ -1,14 +1,35 @@
-import { h, Component } from 'skatejs';
+import { h, Component, prop } from 'skatejs';
 import { Tag } from './Tag';
+import { TagSelector } from './TagSelector';
+import { EventHandler } from '../../manual_definitions/jsx/index';
 
 export class Demo extends Component<void> {
   static get is() { return 'bl-tag-demo' }
 
+  static get props() {
+    return {
+      tags: prop.array()
+    }
+  }
+
+  tags = [
+    { text: 'Tag 1' },
+    { text: 'Tag 2' },
+    { text: 'Tag 3' },
+  ];
+
   private tagClose() {
-    console.log('Tag close emitted', this)
+    console.log( 'Tag close emitted', this )
+    const element = this;
+    element.parentNode.removeChild( element );
+  }
+
+  private tagChange( event ) {
+    // console.log( event );
   }
 
   renderCallback() {
+
     return [
       <style></style>,
       <fieldset>
@@ -20,7 +41,11 @@ export class Demo extends Component<void> {
           <Tag onTagClose={this.tagClose}>Tag three</Tag>
           <Tag onTagClose={this.tagClose}>Tag four</Tag>
         </div>
-
+        <fieldset>
+          <legend>Tag Selector</legend>
+          <TagSelector onTagChange={this.tagChange}
+                       tags={['milk','bread','chocolate']}></TagSelector>
+        </fieldset>
       </fieldset>
     ]
   }
