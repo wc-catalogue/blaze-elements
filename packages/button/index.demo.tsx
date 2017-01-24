7155eaa85c5b3412ff0e052fbd040b5af672f2a0
Martin Hochel <hochelmartin@gmail.com>
2017-01-24 15:35:16 +0100

    refactor(Button): refactor to new structure and tweak demo

--- packages/button/index.demo.tsx
+++ packages/button/index.demo.tsx
@@ -1,26 +1,54 @@
-import { h, Component } from 'skatejs';
-import { Button } from './Button';
+import { h, Component, prop, props } from 'skatejs';
+import { Button } from './';
 
 export class Demo extends Component<void> {
   static get is() { return 'bl-button-demo'; }
 
+  static get props(){
+    return {
+      logger: prop.array()
+    }
+  }
+
+  private logger: string[];
+  private addLogEntry( entry: string ) {
+    return ( ev: Event ) => {
+      props( this, { logger: [ ...this.logger, `${entry} - ${this.logger.length}` ] });
+    }
+  }
+
   renderCallback() {
+    const {logger} = this;
     return [
       <style></style>,
         <fieldset>
           <legend>{Button.is}</legend>
+
+          <bl-button color="brand" onClick={this.addLogEntry('Clicked! bl-button')}>Click me</bl-button>
+
           <Button
             disabled
             color="brand"
+            onClick={this.addLogEntry('Im Disabled!')}
           >Click me</Button>
           <Button
             color="brand"
+            onClick={this.addLogEntry('Clicked Button!')}
           >Click me</Button>
           <Button
             ghost
             color="warning"
+            onClick={this.addLogEntry('Clicked Button ghost!')}
           >Click me</Button>
-        </fieldset>
+        </fieldset>,
+        <div>
+          <h4>Logs:</h4>
+          <ul>
+            {
+              this.logger.map(( log ) => ( <li>{ log }</li> ) )
+            }
+          </ul>
+        </div>
     ];
   }
 }
