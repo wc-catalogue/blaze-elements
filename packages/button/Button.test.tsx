e8787f8b129217948cf3b61f8a84f791b4f61292
Martin Hochel <hochelmartin@gmail.com>
2017-01-27 13:35:07 +0100

    test(button): update button test to follow SG and use expect

--- packages/button/Button.test.tsx
+++ packages/button/Button.test.tsx
@@ -1,18 +1,44 @@
-import { Button } from './';
-
+import * as expect from 'expect';
 import { h, mount } from 'bore';
-import { emit } from 'skatejs';
+
+import { Button } from './index';
 
 describe( Button.is, () => {
 
-  it( 'should render', () => {
+describe( `Custom element`, () => {
+  it( `should be registered`, () => {
+    expect( customElements.get( Button.is ) ).toBe( Button );
+  });
+  it( `should render via JSX IntrinsicElement `, () => {
+    return mount( <bl-button>Hello</bl-button> ).wait( element => {
+      expect( element.node.localName ).toBe( Button.is );
+    });
+  });
+  it( `should render `, () => {
+    return mount( <Button /> ).wait(( element ) => {
+      expect( element.has( '.c-button' ) ).toBe( true );
+    });
+  });
+});
 
-    console.warn('Missing tests for Button!');
+describe( `API`, () => {
 
-    return mount(
-      <Button/>
-    ).wait();
+  describe( `[color]`, () => {
+
+    it( `should render no color class on button by default`, () => {
+      return mount( `<bl-button>huhuhu</bl-button>` ).wait(( element ) => {
+        expect( element.one( 'button' ).node.className ).toBe( 'c-button' );
+      });
+    });
+
+    it( `should render color class on button`, () => {
+      return mount( `<bl-button color="info"></bl-button>` ).wait(( element ) => {
+        expect( element.has( '.c-button--info' ) ).toBe( true );
+      });
+    });
 
   });
+
+});
 
 });
