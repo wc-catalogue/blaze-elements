c5dad2939e6908e8eb86a089d26e858b9f141b4c
Martin Hochel <hochelmartin@gmail.com>
2017-02-06 23:33:13 +0100

    build: update to stable wp 2, wp-ds 2 and remove json-loader

--- webpack.config.js
+++ webpack.config.js
@@ -117,10 +117,6 @@ module.exports = ( env ) => {
           test: /\.svg$/,
           use: [ 'file-loader' ]
         },
-        {
-          test: /\.json$/,
-          use: 'json-loader'
-        }
       ]
     },
 
