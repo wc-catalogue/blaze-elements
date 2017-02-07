45a65513c35d55ff0d7ae8172245982c95a69f3f
Martin Hochel <hochelmartin@gmail.com>
2017-02-07 21:30:12 +0100

    fix(helpers): add proper definitions for Skate mixins and use super.props

--- packages/_helpers/mixins.ts
+++ packages/_helpers/mixins.ts
@@ -1,17 +1,25 @@
-import {prop, ComponentProps} from 'skatejs';
+import { prop, ComponentProps } from 'skatejs';
 import { ColorType } from './colorTypes';
 
 
 /** Any type that can construct *something*. */
-export type Constructable<T> = new (...args: any[]) => T;
+// export type Constructable<T> = new (...args: any[]) => T;
+export interface Constructable<T> {
+  readonly props: ComponentProps<any, any>;
+  readonly observedAttributes: string[];
+  new ( ...args: any[] ): T,
+}
 
 export type ColoredProps = {
   color?: ColorType,
 };
-export function Colored<BC extends Constructable<{}>>(Base: BC) {
-  return class extends Base {
-    static get props(): ComponentProps<any, ColoredProps> {
-      return { color: prop.string<any, ColorType>({attribute: {source: true}}) };
+export function Colored<BC extends Constructable<{}>>( Base: BC ) {
+  return class Colored extends Base {
+    static get props(): ComponentProps<Colored, ColoredProps> {
+      return {
+        ...super.props,
+        color: prop.string<any, ColorType>( { attribute: { source: true } } )
+      };
     }
     color?: ColorType;
   };
@@ -20,10 +28,13 @@ export function Colored<BC extends Constructable<{}>>(Base: BC) {
 export type DisabledProps = {
   disabled?: boolean,
 };
-export function Disabled<BC extends Constructable<{}>>(Base: BC) {
+export function Disabled<BC extends Constructable<{}>>( Base: BC ) {
   return class extends Base {
     static get props(): ComponentProps<any, DisabledProps> {
-      return { disabled: prop.boolean( {attribute: true} ) };
+      return {
+        ...super.props,
+        disabled: prop.boolean( { attribute: true } )
+      };
     }
     disabled?: boolean;
   };
