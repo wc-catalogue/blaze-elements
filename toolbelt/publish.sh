8865e1a5843e6b4565b081953125c2222f39773f
tomas.kral <tomas.kral@embedit.cz>
2017-02-15 12:15:03 +0100

    build: configure travis for semantic release

--- toolbelt/publish.sh
+++ toolbelt/publish.sh
@@ -4,6 +4,7 @@
   git remote set-url origin https://${GH_USERNAME}:${GH_PASSWORD}@github.com/wc-catalogue/blaze-elements.git
   git config user.name "Travis-CI"
   git config user.email "travis@nodemeatspace.com"
+  echo "\n" >> ~/.npmrc
   echo "//registry.npmjs.org/:_authToken=$NPM_KEY" >> ~/.npmrc
-  lerna publish --canary --yes
+  npm run semantic-release
 )
