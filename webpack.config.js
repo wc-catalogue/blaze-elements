97568842f51b3292caad775e73f9a93c68d4cbfd
elmariofredo <mario@vejlupek.cz>
2017-02-21 15:45:50 +0100

    chore: put back wanted changes

--- webpack.config.js
+++ webpack.config.js
@@ -105,6 +105,11 @@ module.exports = ( env ) => {
           test: /\.svg$/,
           use: [ 'file-loader' ]
         },
+        {
+          test: /\.gif|\.jpg$/,
+          use: [ 'url-loader' ]
+        },
+
       ]
     },
 
