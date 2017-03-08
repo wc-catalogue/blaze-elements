ba3ee2693b5b82b9581c345f8e9c4753a0ad6b1a
david.rus <david.rus@embedit.cz>
2017-03-08 12:47:19 +0100

    refactor(tag): follow actual guideline
    affects: @blaze-elements/tag
    
    Closes #269

---
+++ packages/tag/components/Input.tsx
@@ -0,0 +1,5 @@
+import { customElement } from '@blaze-elements/common';
+import Input from '@blaze-elements/input/Input';
+
+@customElement('bl-tag-input')
+export default class TagInput extends Input {}
