d3df494c56c68e7f21964a482d19955b77a4930e
Mario Vejlupek <mario@vejlupek.cz>
2017-03-09 14:33:02 +0100

    refactor(range): align with current style guides (#289)
    affects: @blaze-elements/range
    
    Closes #288

--- packages/range/Range.tsx
+++ packages/range/Range.tsx
@@ -1,56 +1,85 @@
-import { h, Component, prop, emit } from 'skatejs';
+import { h, Component, emit, props } from 'skatejs';
 import styles from './Range.scss';
-import { ColorType, cssClassForColorType, css } from '@blaze-elements/common';
+import {
+  ColorType,
+  cssClassForColorType,
+  css,
+  shadyCssStyles,
+  prop
+} from '@blaze-elements/common';
 
+export type RangeProps = Props & EventProps;
 
-// public
-type RangeProps = Props & EventProps;
-type Props = {
-  value: number | string,
+export type Attrs = {
+  value?: number | string,
   min?: number | string,
   max?: number | string,
   disabled?: boolean,
   color?: ColorType,
 };
-type EventProps = {
+
+export type Props = {
+  value?: number | string,
+  min?: number | string,
+  max?: number | string,
+  disabled?: boolean,
+  color?: ColorType,
+};
+
+export type EventProps = {
   onKeyup?: typeof HTMLElement.prototype.onkeyup,
   onFocus?: typeof HTMLElement.prototype.onfocus,
   onBlur?: typeof HTMLElement.prototype.onblur,
   onChange?: ( ev: CustomEvent ) => void,
 };
 
-export class Range extends Component<RangeProps> {
-  static get is() { return 'bl-range'; }
-  static get props() {
-    return {
-      value: prop.number( {
-        attribute: true
-      } ),
-      min: prop.number( {
-        attribute: true
-      } ),
-      max: prop.number( {
-        attribute: true
-      } ),
-      color: prop.string( {
-        attribute: true
-      } ),
-      disabled: prop.boolean( {
-        attribute: true
-      } ),
-    };
-  }
+export type Events = {};
+
+@shadyCssStyles()
+export default class Range extends Component<RangeProps> {
+
   static get events() {
     return {
       change: 'change'
     };
   }
 
-  color: ColorType;
-  value: number;
-  min = 0;
-  max = 100;
-  disabled: boolean;
+  get css() { return styles; }
+
+  @prop( {
+    type: String,
+    attribute: {
+      source: true
+    }
+  } ) color: ColorType;
+
+  @prop( {
+    type: Number,
+    attribute: {
+      source: true
+    }
+  } ) value: number;
+
+  @prop( {
+    type: Number,
+    attribute: {
+      source: true
+    }
+  } ) min = 0;
+
+  @prop( {
+    type: Number,
+    attribute: {
+      source: true
+    }
+  } ) max = 100;
+
+  @prop( {
+    type: Boolean,
+    attribute: {
+      source: true
+    }
+  } ) disabled: boolean;
 
   inputElement: HTMLInputElement;
 
@@ -65,8 +94,7 @@ export class Range extends Component<RangeProps> {
     const colorClass = cssClassForColorType( 'c-range', color );
     const className = css( 'c-range', colorClass );
 
-    return [
-      <style>{styles}</style>,
+    return (
       <input
         ref={this.setInputElementRef}
         className={className}
@@ -77,19 +105,14 @@ export class Range extends Component<RangeProps> {
         onChange={this.provideValue}
         disabled={disabled}
       />
-    ];
+    );
   }
 
   private provideValue( event: Event ) {
-    this.value = this.inputElement.valueAsNumber;
+    props( this, { value: this.inputElement.valueAsNumber } );
     emit( this, Range.events.change ); // emit change event on root element
   }
   private setInputElementRef( ref: HTMLInputElement ) {
     this.inputElement = ref;
   }
 }
-
-
-customElements.define( Range.is, Range );
-
-
