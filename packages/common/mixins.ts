deef53e39c0e956ac9f4bc723c1e872787359a37
david.rus <david.rus@embedit.cz>
2017-03-09 16:10:17 +0100

    fix(common): repair tsc error & types
    affects: @blaze-elements/common
    
    Closes #293

--- packages/common/mixins.ts
+++ packages/common/mixins.ts
@@ -2,14 +2,15 @@ import { ColorType } from './colorTypes';
 import { IS_DEV } from './environment';
 import { scopeCss, Constructable } from './utils';
 import { prop } from './decorators';
+import { MixinClass } from './static-definitions/types';
 
 export type ColoredProps = {
   color?: ColorType,
 };
-export function Colored<BC extends Constructable<{}>>( Base: BC ) {
+export function Colored<BC extends Constructable<{}>>( Base: BC ): MixinClass<BC, ColoredProps> {
   class ColoredCmp extends Base {
     @prop( { type: String, attribute: { source: true } } ) color?: ColorType;
-  };
+  }
 
   return ColoredCmp;
 }
@@ -17,17 +18,20 @@ export function Colored<BC extends Constructable<{}>>( Base: BC ) {
 export type DisabledProps = {
   disabled?: boolean,
 };
-export function Disabled<BC extends Constructable<{}>>( Base: BC ) {
+export function Disabled<BC extends Constructable<{}>>( Base: BC ): MixinClass<BC, DisabledProps> {
   class DisabledCmp extends Base {
     @prop( { type: Boolean, attribute: true } ) disabled?: boolean;
-  };
+  }
 
   return DisabledCmp;
 }
 
 
-
-export function Css<BC extends Constructable<{}>>( Base: BC ) {
+export type CssProps = {
+  readonly css: string,
+  readonly shadyCss: void | string,
+};
+export function Css<BC extends Constructable<{}>>( Base: BC ): MixinClass<BC, CssProps> {
   return class extends Base {
     readonly css: string;
 
