2522cb8b158aed99adf3bb1a058ec88a717462d6
Mario Vejlupek <mario@vejlupek.cz>
2017-03-09 09:44:45 +0100

    refactor(accordion): align with current style guides (#262)
    Closes #254

--- packages/accordion/index.ts
+++ packages/accordion/index.ts
@@ -1,1 +1,19 @@
-import './Accordion';
+
+import { customElement, GenericTypes } from '@blaze-elements/common';
+import RawAccordion, { AccordionProps, Attrs, Events } from './Accordion';
+
+const Accordion = customElement( 'bl-accordion' )( RawAccordion ) as typeof RawAccordion;
+
+export {
+  Accordion
+};
+
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      'bl-accordion': GenericTypes.IntrinsicCustomElement<AccordionProps>
+      & GenericTypes.IntrinsicBoreElement<Attrs, Events>
+    }
+  }
+}
